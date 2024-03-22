ec3acfb61207fbd42b6f28161ca3e07094f3a481
dkwhyd <dikiwhyd99@gmail.com>
2024-03-22 13:00:41 +0700

    set launchscreen for first screen and getting data user login

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:events/screens/event_screen.dart';
+import 'package:events/screens/launch_screen.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'firebase_options.dart';
@@ -16,12 +16,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Events',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const EventScreen(),
+      home: const LaunchScreen(),
     );
   }
 }
