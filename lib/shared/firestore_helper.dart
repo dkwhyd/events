c2304849b835b954827361f908f4b4f73aa9ae4b
dkwhyd <dikiwhyd99@gmail.com>
2024-03-23 12:24:00 +0700

    fix postion params

--- lib/shared/firestore_helper.dart
+++ lib/shared/firestore_helper.dart
@@ -6,12 +6,12 @@ class FireStoreHelper {
   static final FirebaseFirestore db = FirebaseFirestore.instance;
 
   static Future addFavorite(EventDetail eventDetail, String uid) {
-    Favorite fav = Favorite(null, uid, uid);
+    Favorite fav = Favorite(null, eventDetail.id, uid);
     var result = db
         .collection('favorite')
         .add(fav.toMap())
         .then((value) => print(value))
-      .catchError((error) => print(error));
+        .catchError((error) => print(error));
 
     return result;
   }
