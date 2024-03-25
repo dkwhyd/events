b664f619cedf27af5add9e8e8d3987d668a52cf6
dkwhyd <dikiwhyd99@gmail.com>
2024-03-25 14:45:58 +0700

    clean

--- lib/models/favorite.dart
+++ lib/models/favorite.dart
@@ -1,28 +1,24 @@
 class Favorite {
-  String? _id;
+  String? id;
   String? _eventId;
   String? _userId;
 
-  Favorite(this._id, this._eventId, this._userId);
+  Favorite(this.id, this._eventId, this._userId);
 
   Favorite.map(snapshot) {
     Map<String, dynamic> data = snapshot.data()!;
-    _id = snapshot.id;
+    id = snapshot.id;
     _eventId = data['eventId'];
     _userId = data['userId'];
   }
 
   String? get eventId => _eventId;
-  String? get userId => _userId;
-
-  String? get id => _id;
-
-  set id(String? value) => _id = value; // Setter for 'id'
+  String? get userId => _userId; // Setter for 'id'
 
   Map<String, dynamic> toMap() {
     Map<String, dynamic> map = <String, dynamic>{};
-    if (_id != null) {
-      map['id'] = _id;
+    if (id != null) {
+      map['id'] = id;
     }
     map['eventId'] = _eventId;
     map['userId'] = _userId;
